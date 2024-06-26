6f9fe81c9b0c801ea8c2755e3055449e304c834a
jackson makl <jackson.makl@dataiku.com>
2024-06-26 22:33:26 +0000

    f

---
+++ install_jupyter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the path to the virtual environment
+VENV_DIR="/home/ubuntu/data_flow_tool/airflow_venv"
+
+# Check if the virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Virtual environment directory $VENV_DIR does not exist."
+  exit 1
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install Jupyter Notebook
+pip install jupyter
+
+# Deactivate the virtual environment
+deactivate
+
+echo "Jupyter Notebook has been installed in the virtual environment $VENV_DIR"
---
+++ launch_jupyter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Activate the virtual environment if necessary
+source /home/ubuntu/data_flow_tool/airflow_venv/bin/activate
+
+# Generate a Jupyter configuration file if it doesn't exist
+jupyter notebook --generate-config -y
+
+# Update the Jupyter configuration to disable token authentication
+echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = ''" >> ~/.jupyter/jupyter_notebook_config.py
+
+# Launch Jupyter Notebook in the specified directory and port
+jupyter notebook --notebook-dir=/home/ubuntu/data_flow_tool/dags --port=8888 --ip=0.0.0.0 --allow-root --no-browser
