a0b4267b5c33377d2ff4dd6b0e6423492b716019
jackson makl <jackson.makl@dataiku.com>
2024-06-26 18:39:41 +0000

    test

--- template_folder/scripts/script1.py
+++ template_folder/scripts/script1.py
@@ -1,5 +1,11 @@
 # script1.py
 def script1():
     print("Executing Script 1")
+    # Create or open a text file named hello_world.txt in write mode
+    with open('hello_world.txt', 'w') as file:
+        # Write the string "Hello World" to the file
+        file.write("Hello World")
+
+    print("File has been created and written to successfully.")
 
 script1()
