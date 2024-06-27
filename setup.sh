cf9a34554e3f181c376b08a4fd1ff45e0c1515f7
Jackson Makl <jackson.makl@dataiku.com>
2024-06-26 20:03:17 -0400

    j

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 ./airflow_install.sh
 ./docker_install.sh
+./jupyter_install.sh
 ./create_dag.sh HELLO_WORLD
 ./airflow_start.sh
 ./airflow_stop.sh
 ./docker_image.sh
 ./docker_stop.sh
 ./docker_start.sh
+./jupyter_start.sh
