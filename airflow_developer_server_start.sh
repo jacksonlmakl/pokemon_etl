6c0592a87e8e02ff70f85b41c9454d34d584e08a
Jackson Makl <jackson.makl@dataiku.com>
2024-06-27 01:56:42 -0400

    f

---
+++ airflow_developer_server_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# airflow_development_server_start.sh
+
+# Get the current working directory
+CURRENT_DIR=$(pwd)
+
+# Set AIRFLOW_HOME to current directory
+export AIRFLOW_HOME="$CURRENT_DIR/airflow_home"
+
+# Activate the virtual environment
+source "$CURRENT_DIR/airflow_venv/bin/activate"
+
+# Start the Airflow webserver
+echo "Starting Airflow webserver..."
+airflow webserver -p 8081 &
+
+# Start the Airflow scheduler
+echo "Starting Airflow scheduler..."
+airflow scheduler &
+
+echo "Airflow webserver and scheduler started."
+
