884bdb79570b287b2d389ffbfbb9265337b16165
Jackson Makl <jackson.makl@dataiku.com>
2024-06-28 13:51:43 -0400

    c

--- airflow_start.sh
+++ airflow_start.sh
@@ -11,6 +11,13 @@ export AIRFLOW_HOME="$CURRENT_DIR/airflow_home"
 # Activate the virtual environment
 source "$CURRENT_DIR/airflow_venv/bin/activate"
 
+# Find every requirements.txt file in the dags directory and its subdirectories
+find dags -name 'requirements.txt' | while read requirements_file; do
+    echo "Installing packages from $requirements_file"
+    pip install -r "$requirements_file"
+done
+
+
 # Start the Airflow webserver
 echo "Starting Airflow webserver..."
 airflow webserver -p 8080 &
