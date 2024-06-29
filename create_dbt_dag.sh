012f1245fbb7af3defecc8709c51ea01491ea60d
Jackson Makl <jackson.makl@dataiku.com>
2024-06-28 23:00:20 -0400

    test

--- create_dbt_dag.sh
+++ create_dbt_dag.sh
@@ -23,9 +23,11 @@ mkdir -p "$EXTERNAL_DAGS_DIR/$DAG_NAME"
 
 # Create DBT project structure
 DBT_PROJECT_DIR="$EXTERNAL_DAGS_DIR/$DAG_NAME/dbt_project"
-# mkdir -p "$DBT_PROJECT_DIR/models/staging"
-# mkdir -p "$DBT_PROJECT_DIR/models/marts"
+
 mkdir -p "$DBT_PROJECT_DIR/models"
+mkdir -p "$DBT_PROJECT_DIR/models/staging"
+mkdir -p "$DBT_PROJECT_DIR/models/marts"
+mkdir -p "$DBT_PROJECT_DIR/models/sources"
 mkdir -p "$DBT_PROJECT_DIR/seeds"
 # mkdir -p "$DBT_PROJECT_DIR/snapshots"
 mkdir -p "$DBT_PROJECT_DIR/macros"
@@ -38,6 +40,10 @@ cp "$CURRENT_DIR/template_dbt/dbt_project.yml" "$DBT_PROJECT_DIR/dbt_project.yml
 cp "$CURRENT_DIR/template_dbt/profiles.yml" "$DBT_PROJECT_DIR/profiles.yml"
 cp "$CURRENT_DIR/template_dbt/models" "$DBT_PROJECT_DIR/models"
 # cp "$CURRENT_DIR/template_dbt/models/marts/dim_customers.sql" "$DBT_PROJECT_DIR/models/marts/dim_customers.sql"
+cp "$CURRENT_DIR/template_dbt/models/marts/core_pokemon.sql" "$DBT_PROJECT_DIR/models/marts/core_pokemon.sql"
+cp "$CURRENT_DIR/template_dbt/models/staging/stg_pokemon.sql" "$DBT_PROJECT_DIR/models/staging/stg_pokemon.sql"
+cp "$CURRENT_DIR/template_dbt/models/sources/pokemon.sql" "$DBT_PROJECT_DIR/models/sources/pokemon.sql"
+
 cp "$CURRENT_DIR/template_dbt/seeds/my_seed_data.csv" "$DBT_PROJECT_DIR/seeds/my_seed_data.csv"
 # cp "$CURRENT_DIR/template_dbt/snapshots/snapshot_orders.sql" "$DBT_PROJECT_DIR/snapshots/snapshot_orders.sql"
 cp "$CURRENT_DIR/template_dbt/macros/my_custom_macro.sql" "$DBT_PROJECT_DIR/macros/my_custom_macro.sql"
